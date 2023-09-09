e7cc4b42c30a4966ff91b553d7059662ed4a8b7c
10Valentin01 <115719978+10Valentin01@users.noreply.github.com>
2023-09-09 17:38:27 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,3 +1,36 @@
+// const cart = {
+//     items: [], // Массив товаров в корзине
+//     addItem: function (product) {
+//         // Функция для добавления товара в корзину
+//         // Добавьте товар в массив items
+//     },
+//     increaseQuantity: function (product) {
+//         // Функция для увеличения количества товара в корзине
+//         // Увеличьте количество товара в массиве items
+//     },
+//     getTotalPrice: function () {
+//         // Функция для вычисления общей стоимости товаров в корзине
+//         // Пройдитесь по массиву items и сложите стоимость каждого товара
+//     },
+// };
+
+// document.querySelectorAll(".add-to-cart-button").forEach((button, index) => {
+//     button.addEventListener("click", () => {
+//         const product = products[index]; // Получите товар из массива товаров, например, по индексу
+//         cart.addItem(product); // Добавьте товар в корзину
+//         // Обновите отображение количества товаров в корзине
+//     });
+// });
+
+// document.querySelectorAll(".increase-quantity-button").forEach((button, index) => {
+//     button.addEventListener("click", () => {
+//         const product = cart.items[index]; // Получите товар из корзины по индексу
+//         cart.increaseQuantity(product); // Увеличьте количество товара в корзине
+//         // Обновите отображение количества товаров и общей стоимости в корзине
+//     });
+// });
+
+
 // Функция для загрузки товаров из JSON-файла
 function loadProducts(category) {
     fetch('products.json') // Загрузка JSON-файла
