1764d7251bb86e139641bad1407d34e6709e3413
10Valentin01 <115719978+10Valentin01@users.noreply.github.com>
2023-09-09 13:00:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ function redirectToCategory(category) {
     // Формируем URL для перехода на страницу товаров, передавая выбранную категорию
     const url = `products.html?category=${category}`;
     window.location.href = url;
+    console.log(categoryData);
 }
