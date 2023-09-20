4353a6968b13d4f739026614dcd6569782d84858
10Valentin01 <115719978+10Valentin01@users.noreply.github.com>
2023-09-20 21:57:57 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Функция для перехода на страницу товаров с выбранной категорией
-function redirectToCategory(category) {
-    // Формируем URL для перехода на страницу товаров, передавая выбранную категорию
-    const url = `products.html?category=${category}`;
-    window.location.href = url;
-    console.log(categoryData);
-}
